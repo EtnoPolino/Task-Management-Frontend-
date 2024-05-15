96ffb02cf74e4a9ff7ac58d4c422f7f2515b8916
Etno Polino <einshoch66@gmail.com>
2024-05-15 02:09:01 +0200

    init + creation des modules + components & Navbar

---
+++ task_angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, SignupComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoAngularMaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
