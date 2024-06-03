81e9e19f1fc16265e0d7d392858b1241a5515e1c
Etno Polino <einshoch66@gmail.com>
2024-06-03 18:31:07 +0200

    Create Storage Service & Role base navigation

--- task_angular/src/app/auth/components/login/login.component.ts
+++ task_angular/src/app/auth/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { StorageService } from '../../services/storage/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +17,8 @@ export class LoginComponent {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private router: Router
   ) {
     this.loginForm = this.fb.group({
       email: [null, [Validators.required, Validators.email]],
@@ -33,6 +36,18 @@ export class LoginComponent {
     this.authService.login(this.loginForm.value).subscribe((response) => {
       console.log(response);
       if (response.userId != null) {
+        const user = {
+          id: response.userId,
+          role: response.userRole,
+        };
+        StorageService.saveUser(user);
+        StorageService.saveToken(response.jwt);
+
+        if (StorageService.isAdminLoggedIn()) {
+          this.router.navigateByUrl('/admin/dashboard');
+        } else if (StorageService.isEmployeeLoggedIn()) {
+          this.router.navigateByUrl('/employee/dashboard');
+        }
         this.snackbar.open('Log in successful', 'Close', { duration: 5000 });
       } else {
         this.snackbar.open('Invalid credential', 'Close', {
