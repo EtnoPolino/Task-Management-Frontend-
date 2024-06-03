0088c1c5f3727245c1cb3d17f493120d3d95c5cd
Etno Polino <einshoch66@gmail.com>
2024-06-03 04:52:57 +0200

    create a signup & login reactive form

--- task_angular/src/app/auth/components/login/login.component.ts
+++ task_angular/src/app/auth/components/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  hidePassword = true;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
