064cf7e208702bdebcf73d66a342a7d2083ad192
Etno Polino <einshoch66@gmail.com>
2024-06-05 05:45:02 +0200

    creation of get employees api [frontend]

--- task_angular/src/app/modules/admin/admin.module.ts
+++ task_angular/src/app/modules/admin/admin.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { PostTaskComponent } from './components/post-task/post-task.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
+  declarations: [DashboardComponent, PostTaskComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
 })
-export class AdminModule { }
+export class AdminModule {}
